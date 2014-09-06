09e6a3a83fd046927e1315b8b800301cb7ca8337
Jesus Figueroa <jesus.figueroa@crowdint.com>
2014-09-05 19:26:20 -0500

    Splits file by line breaks

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"./mypackage"
+	"strings"
 )
 
 // The Bla1 example tests if the output matches
@@ -60,15 +60,30 @@ func ExampleMap() {
 
 func ExampleReadFile() {
 	// http://golang.org/pkg/io/ioutil/#ReadFile
-	bytes, readError := ioutil.ReadFile("numbers.txt")
+	bytes, readError := ioutil.ReadFile("numbers_original.txt")
 
 	if readError != nil {
 		panic(readError)
 	}
 
-	fmt.Println(bytes)
-	fmt.Println(len(bytes))
-	fmt.Println(string(bytes))
+	lines:= strings.Split(string(bytes), "\n")
+	key:= 0
+
+	var number_lines [3]string
+
+	for _, val:= range lines {
+		if key == 3 {
+			GetNumber(number_lines)
+			key = 0
+		} else {
+			number_lines[key] = val
+			key++
+		}
+	}
+
+//	fmt.Println(bytes)
+//	fmt.Println(len(bytes))
+//	fmt.Println(string(bytes))
 
 	// Output:
 	// [32 95 10 124 32 124 10 124 95 124 10 10 10]
@@ -77,6 +92,10 @@ func ExampleReadFile() {
 	//	| |
 	//	|_|
 
+}
+
+func GetNumber (values [3]string) {
+	fmt.Println(values)
 }
 
 func ExampleFunctions() {
